41f4325f0309953ceb89f1a119e91fd8c3a928f3
lucasbarroscode <lucasbabezerra@gmail.com>
2022-01-11 21:49:03 -0300

    Firtst Commit

---
+++ src/main/java/io/github/lucasbarroscode/clientes/model/entity/Cliente.java
@@ -0,0 +1,33 @@
+package io.github.lucasbarroscode.clientes.model.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 150)
+	private String nome;
+	
+	@Column(nullable = false, length = 11)
+	private String cpf;
+	
+	@Column
+	private LocalDate dataCadastro;
+	
+
+
+}

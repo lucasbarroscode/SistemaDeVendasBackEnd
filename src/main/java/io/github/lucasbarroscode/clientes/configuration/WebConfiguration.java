107d130e57adf2b43e5378aa73f9ed90217d6157
lucasbarroscode <lucasbabezerra@gmail.com>
2022-01-13 23:31:25 -0300

    corrigindo o bug para a conexao com o banco h2 em memoria

---
+++ src/main/java/io/github/lucasbarroscode/clientes/configuration/WebConfiguration.java
@@ -0,0 +1,5 @@
+package io.github.lucasbarroscode.clientes.configuration;
+
+public class WebConfiguration {
+
+}
--- src/main/java/io/github/lucasbarroscode/clientes/model/entity/Servico.java
+++ src/main/java/io/github/lucasbarroscode/clientes/model/entity/Servico.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 import lombok.Data;
 

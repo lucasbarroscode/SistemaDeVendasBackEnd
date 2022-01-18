0a05b0260910af01eb56065b745979ea941eec9f
lucasbarroscode <lucasbabezerra@gmail.com>
2022-01-17 23:25:00 -0300

    deletando o cliente pelo id

--- src/main/java/io/github/lucasbarroscode/clientes/rest/ClienteController.java
+++ src/main/java/io/github/lucasbarroscode/clientes/rest/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.lucasbarroscode.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,20 @@ public class ClienteController {
 	public Cliente acharPorId(@PathVariable Integer id) {
 		return repository
 				.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Integer id) {
+		//repository.deleteById(id);
+		
+		repository
+		.findById(id)
+		.map(cliente -> {
+			repository.delete(cliente);
+			return Void.TYPE;
+		})
+		.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }

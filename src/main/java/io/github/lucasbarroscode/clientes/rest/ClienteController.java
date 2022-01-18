5141df92d01ad5ddad122804f9e4cd575299b51a
lucasbarroscode <lucasbabezerra@gmail.com>
2022-01-17 23:19:20 -0300

    obtendo as informacoes do cliente por id

--- src/main/java/io/github/lucasbarroscode/clientes/rest/ClienteController.java
+++ src/main/java/io/github/lucasbarroscode/clientes/rest/ClienteController.java
@@ -2,11 +2,14 @@ package io.github.lucasbarroscode.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.lucasbarroscode.clientes.model.entity.Cliente;
 import io.github.lucasbarroscode.clientes.model.repository.ClienteRepository;
@@ -22,6 +25,12 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente salvar(@RequestBody Cliente cliente) {
 		return repository.save(cliente);
+	}
+	
+	@GetMapping("{id}")
+	public Cliente acharPorId(@PathVariable Integer id) {
+		return repository
+				.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }

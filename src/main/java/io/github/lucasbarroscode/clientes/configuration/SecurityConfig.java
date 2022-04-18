4d7d2dd723c5e4350305266596b622722af006b4
lucasbarroscode <lucasbabezerra@gmail.com>
2022-04-17 23:11:53 -0300

    adicionando dependencias de seguranca e testando as configs

---
+++ src/main/java/io/github/lucasbarroscode/clientes/configuration/SecurityConfig.java
@@ -0,0 +1,15 @@
+package io.github.lucasbarroscode.clientes.configuration;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+
+}

c2f41d3da5b43d6b8a758cd9fd85d6569eec8268
lucasbarroscode <lucasbabezerra@gmail.com>
2022-04-24 20:47:58 -0300

    configurando o spring security

--- src/main/java/io/github/lucasbarroscode/clientes/configuration/SecurityConfig.java
+++ src/main/java/io/github/lucasbarroscode/clientes/configuration/SecurityConfig.java
@@ -1,15 +1,36 @@
 package io.github.lucasbarroscode.clientes.configuration;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
-@EnableAuthorizationServer
-@EnableResourceServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-
+	//quais sao os usuariois da aplicacao e suas senhas
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication().withUser("Fulano").password("123").roles("USER");
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.cors()
+		.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
 }

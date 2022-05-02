9cfd891bf6a7d3caa60a9dbdcc080f3b7d99457a
lucasbarroscode <lucasbabezerra@gmail.com>
2022-05-01 21:44:47 -0300

    autenticando com os usuarios da base de dados

--- src/main/java/io/github/lucasbarroscode/clientes/configuration/SecurityConfig.java
+++ src/main/java/io/github/lucasbarroscode/clientes/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.github.lucasbarroscode.clientes.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,15 +9,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import io.github.lucasbarroscode.clientes.service.UsuarioService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UsuarioService usuarioService;
 	
 	//quais sao os usuariois da aplicacao e suas senhas
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-		.inMemoryAuthentication().withUser("Fulano").password("123").roles("USER");
+			.userDetailsService(usuarioService)
+			.passwordEncoder(passwordEncoder());
 	}
 	
 	@Bean
@@ -32,5 +41,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.cors()
 		.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
